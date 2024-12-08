b892a64d932744cfcebe8565a70d6f884d559b73
M-D-Nadeem <149703432+M-D-Nadeem@users.noreply.github.com>
2024-12-08 13:15:58 +0530

    first commit

--- src/routers/axiosInstance.js
+++ src/routers/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance=axios.create()
 
-axiosInstance.defaults.baseURL="http://16.171.1.181:3000/"
+axiosInstance.defaults.baseURL="http://localhost:3000/"
 axiosInstance.defaults.withCredentials=true
 
 export default axiosInstance
